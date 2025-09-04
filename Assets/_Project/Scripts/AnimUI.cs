ed9eaa645c7887c93992a4b6166e17ae0775d7ab
RaphaelBattistel <raphaelbattistel@gmail.com>
2025-09-04 13:58:03 +0200

    Police d'écriture + modif AnimUI

--- Assets/_Project/Scripts/AnimUI.cs
+++ Assets/_Project/Scripts/AnimUI.cs
@@ -1,7 +1,15 @@
+using System.Collections;
+using TMPro;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AnimUI : MonoBehaviour
 {
+
+    [SerializeField] private float countdown = 20;
+    [SerializeField] private TextMeshProUGUI display;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +19,25 @@ public class AnimUI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        StartCoroutine("Countdown");
+
+    }
+    
+    IEnumerator Countdown()
+    {
+        if (countdown > 0)
+        {
+            countdown -= Time.deltaTime;
+            display.text = "" + (int)countdown;
+        }
+
+        else
+        {
+            display.text = "GO !";
+            yield return new WaitForSeconds(1f);
+        }
+            
+
         
     }
 }
