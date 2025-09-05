e269d0d6a56108c8d80c78f25b03904467f3ed2a
Enzo Bossé <bosseenzo6@gmail.com>
2025-09-05 11:25:17 +0200

    [ADD] Startscene final

--- Assets/_Project/Scripts/Managers/UIManagerSceneStart.cs
+++ Assets/_Project/Scripts/Managers/UIManagerSceneStart.cs
@@ -1,13 +1,17 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class UIManagerSceneStart : MonoBehaviour
 {
     [SerializeField] private List<GameObject> _playerSprites;
     private int index = 0;
+
+    public UnityEvent UnityOnEnableSprite;
     
     public void EnablePlayerSprites()
     {
+        UnityOnEnableSprite?.Invoke();
         _playerSprites[index].SetActive(true);
         if (index < 1)
         {
